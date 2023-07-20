49a0fc06756cb92412274dbd88bef7e95d16ecbc
ibrahimsow79 <ibrahimsow79@gmail.com>
2023-07-20 08:53:27 +0200

    First  commit

---
+++ backend.tf
@@ -0,0 +1,10 @@
+#configuring the backend to store the tfstate
+/*
+terraform {
+  backend "s3" {
+    region     = "eu-west-1"
+    bucket     = "GIVE THE BUCKET NAME"
+    key        = "terraform.tfstate"
+  }
+}
+*/
